498aa3c79b2f8b069c7e864d853cfa34a7cf8a6f
Mollie Ball <mollieball@squarepeg.ie>
2020-02-28 12:47:17 +0000

    Create CityDetails component

---
+++ src/components/DetailsDisplay/DetailsDisplay.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./DetailsDisplay.css";
+
+class DetailsDisplay extends React.Component {
+  formatValue = (value, type) => {
+    switch (type) {
+      case "currency_dollar":
+        return `$${value}`;
+
+      case "float":
+        return Number(value).toFixed(2);
+
+      case "percent":
+        return `${Number(value * 100).toFixed(2)}%`;
+
+      default:
+        return value;
+    }
+  };
+
+  render() {
+    const { label, data } = this.props;
+
+    return (
+      <Row className={"DetailsDisplay"}>
+        {/*
+        NOTE: waiting to pull Title component to use here
+        <Col>
+          <h1>{label}</h1>
+        </Col> */}
+        <Col>
+          <ul>
+            {data.map(breakDown => (
+              <li className="text-break p-1 m-3">
+                <span className="title">
+                  {breakDown.label.replace(" [Teleport score]", "")}:{" "}
+                </span>
+                <strong>
+                  {this.formatValue(
+                    breakDown[`${breakDown.type}_value`],
+                    breakDown.type
+                  )}{" "}
+                </strong>
+              </li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default DetailsDisplay;
---
+++ src/components/ScoreDisplay/ScoreDisplay.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./ScoreDisplay.css";
+
+function ScoreDisplay({ name, score }) {
+  let variant;
+  if (score <= 3) {
+    variant = "danger";
+  } else if (score <= 6) {
+    variant = "warning";
+  } else {
+    variant = "success";
+  }
+
+  return (
+    <Row className="ScoreDisplay p-2 align-items-center">
+      <Col className={`text-${variant}`}>
+        <h2>
+          <strong>{name}</strong>
+        </h2>
+      </Col>
+      <Col>
+        <ProgressBar
+          variant={variant}
+          now={score * 10}
+          label={`${Number(score * 10).toFixed(0)}%`}
+        />
+      </Col>
+    </Row>
+  );
+}
+
+export default ScoreDisplay;
