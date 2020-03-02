34dbd8a36882cd0accded9bf814bade9aed9ac2c
Andrea de Roeck <deroeck.andrea@gmail.com>
2020-03-02 16:21:34 +0000

    Change app layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import CityHeader from "./components/CityHeader/CityHeader";
 import { Switch, Route } from "react-router-dom";
 import CityDetails from "./components/CityDetails/CityDetails";
@@ -7,12 +10,20 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <React.Fragment>
-      <CityHeader />
-      <Switch>
         <Route exact path="/" component={() => null} />
-        <Route path="/map" component={() => null} />
-        <Route path="/city/:cityName" component={CityDetails} />
-      </Switch>
+      <Container className="container-style mt-3" fluid>
+        <Row>
+          <Col sm={12} md={12} lg={2}>
+            <CityHeader />
+          </Col>
+          <Col sm={12} md={12} lg={10} className="mt-3 pl-0 pr-0">
+            <Switch>
+              <Route path="/map" component={() => null} />
+              <Route path="/city/:cityName" component={CityDetails} />
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
       <Footer />
     </React.Fragment>
   );
