c729ac3facfa994cc1e26c9aa71b800f29b6b6df
Andrea de Roeck <deroeck.andrea@gmail.com>
2020-03-02 16:30:01 +0000

    Add Home component to app Switch

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import CityHeader from "./components/CityHeader/CityHeader";
 import { Switch, Route } from "react-router-dom";
 import CityDetails from "./components/CityDetails/CityDetails";
 import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <React.Fragment>
-        <Route exact path="/" component={() => null} />
       <Container className="container-style mt-3" fluid>
         <Row>
           <Col sm={12} md={12} lg={2}>
@@ -18,6 +18,7 @@ function App() {
           </Col>
           <Col sm={12} md={12} lg={10} className="mt-3 pl-0 pr-0">
             <Switch>
+              <Route exact path="/" component={Home} />
               <Route path="/map" component={() => null} />
               <Route path="/city/:cityName" component={CityDetails} />
             </Switch>
